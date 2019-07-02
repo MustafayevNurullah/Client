47b8158da2a2abe14dfa4ec4d04752fe8b172e80
nurlan3295 <nurlan3295@gmail.com>
2019-07-02 21:59:40 +0400

    Add project files.

---
+++ Client1/Command/SendCommand.cs
@@ -0,0 +1,34 @@
+﻿using Client1.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Client1.Command
+{
+    public class SendCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        ClientViewModel clientViewModel;
+        public SendCommand(ClientViewModel clientViewModel)
+        {
+            this.clientViewModel = clientViewModel;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Task senderTask = Task.Run(() =>
+            {
+                clientViewModel.socket.Send(Encoding.ASCII.GetBytes(clientViewModel.CurrentText));
+
+            });
+        }
+    }
+}
---
+++ Client1/ViewModel/ClientViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Client1.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client1.ViewModel
+{
+    public class ClientViewModel : BaseViewModel
+    {
+       public SendCommand sendCommandCMD { get; set; }
+        public Socket socket;
+        public ClientViewModel()
+        {
+            sendCommandCMD = new SendCommand(this);
+            IPEndPoint ıPEndPoint = new IPEndPoint(IPAddress.Parse("10.1.16.13"), 1031);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ıPEndPoint);
+        }
+        private string client;
+        public string Client
+        {
+
+            get
+            {
+                return client;
+            }
+            set
+            {
+                client = value;
+                OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs(nameof (Client)));
+            }
+        }
+        string currentText;
+        public string CurrentText
+         {
+            get
+            {
+                return currentText;
+            }
+
+            set
+            {
+                currentText = value;
+                OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs(nameof(CurrentText)));
+            }
+
+        }
+
+    }
+}
